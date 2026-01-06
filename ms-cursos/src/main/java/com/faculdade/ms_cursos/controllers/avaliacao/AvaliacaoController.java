a2350e7771c655b10ceb575196fd1c3e5843e558
Rafael Caxixi <rafael.fuzeti@sptech.school>
2026-01-06 18:49:30 -0300

    Ajuste no buscar avaliacao por id

--- ms-cursos/src/main/java/com/faculdade/ms_cursos/controllers/avaliacao/AvaliacaoController.java
+++ ms-cursos/src/main/java/com/faculdade/ms_cursos/controllers/avaliacao/AvaliacaoController.java
@@ -52,7 +52,7 @@ public class AvaliacaoController {
             description = "Retorna uma avalição. Alunos só podem ver as suas.")
     @ApiResponse(responseCode = "200", description = "Avaliação encontrada.")
     @ApiResponse(responseCode = "403", description = "Proibido. Usuário tenta acessar avaliação de terceiros.")
-    @PreAuthorize("hasAnyAuthority('ALUNO')")
+    @PreAuthorize("hasAnyAuthority('PROFESSOR')")
     public ResponseEntity<AvaliacaoResponseDTO> buscarConsultaPorId(
             @Parameter(description = "ID da consulta.") @PathVariable UUID id) {
 
